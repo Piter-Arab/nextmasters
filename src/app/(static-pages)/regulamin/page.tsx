83569b683dbf45c0271be76a655765bdb6065b4f
Piter-Arab <piotr.bara@icloud.com>
2024-04-02 19:43:00 +0200

    2.2-GrupyStron

---
+++ src/app/(static-pages)/regulamin/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TermsOfService = () => {
+  return (
+    <>
+      <h3 className="text-4xl mb-8 font-extrabold">Regulamin</h3>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at euismod
+        metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+        rhoncus consectetur nulla, rutrum congue est finibus et. Cras volutpat
+        faucibus rutrum. Nulla aliquet condimentum neque. Mauris lectus eros,
+        pulvinar a fermentum eu, commodo eu arcu. Pellentesque tristique sem
+        arcu, quis tincidunt dolor finibus vitae. Ut massa purus, viverra nec
+        bibendum in, consequat at mi. Sed iaculis lacinia lorem eu luctus. Sed
+        semper lobortis orci, eu sagittis neque. Nam ultricies tellus a
+        consequat eleifend. Sed arcu erat, accumsan non ligula ac, convallis
+        fringilla leo. Nullam consectetur purus sit amet.
+      </p>
+    </>
+  );
+};
+
+export default TermsOfService;
---
+++ src/constants/Navigation.ts
@@ -0,0 +1,10 @@
+export const navigation = {
+  main: [
+    { name: "About", href: "#" },
+    { name: "Blog", href: "#" },
+    { name: "Jobs", href: "#" },
+    { name: "Press", href: "#" },
+    { name: "Accessibility", href: "#" },
+    { name: "Polityka Prywatności", href: "/polityka-prywatnosci" },
+  ],
+};
